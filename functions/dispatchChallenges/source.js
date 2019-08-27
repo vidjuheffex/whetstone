99a21d9d87caee93419509821a3763338c8f4f8c
julian herrera <julian.herrera@live.com>
2019-08-27 13:34:03 -0500

    Intial Commit - Add exiting app to version control

---
+++ functions/dispatchChallenges/source.js
@@ -0,0 +1,38 @@
+function calcTime(offset) {
+  // create Date object for current location
+  d = new Date();
+  
+  // convert to msec
+  // add local time zone offset
+  // get UTC time in msec
+  utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+  
+  // create new Date object for different city
+  // using supplied offset
+  nd = new Date(utc + (3600000*offset));
+  
+  // return time as a string
+  return nd.getHours();
+}
+
+exports = async function() {
+  const mongodb = context.services.get("whetstone-stitch");
+  const whetstonedb = mongodb.db("whetstone-db");
+  const teamsCol = whetstonedb.collection("teams");
+  const challengesCol = whetstonedb.collection("challenges");
+  
+  const teams = await teamsCol.find().toArray();
+  
+  const [{challenge: text}] = await challengesCol.aggregate([{ $sample: { size: 1 } }]).toArray();
+  
+  teams.forEach((team) => {
+
+    if (calcTime(team.gmtOffset) === 11) {
+      context.http.post({
+        url: team.incomingWebhook.url,
+        body: {text},
+        encodeBodyAsJSON: true,
+      });
+    }
+  });
+}
---
+++ services/slack/incoming_webhooks/incoming/source.js
@@ -0,0 +1,23 @@
+exports = async function(payload) {
+  const mongodb = context.services.get("whetstone-stitch");
+  const whetstonedb = mongodb.db("whetstone-db");
+  const challengesCol = whetstonedb.collection("challenges");
+  
+  const command = payload && payload.query && payload.query.command
+  
+  if (command) {
+    switch (command) {
+      case "/challenge":
+        const [{challenge: text}] = await challengesCol.aggregate([{ $sample: { size: 1 } }]).toArray();
+        
+        
+        return { text };
+      default:
+        return {
+          text: "Unrecognized"
+        };
+    }  
+  }
+  
+  return payload;
+};
